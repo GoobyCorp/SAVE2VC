7d9fe8b5c5403df534628d3d8e8aa003b2ddd96e
John <pls@gooby.co>
2020-03-12 04:23:56 -0400

    Added additional commands

--- GBA2WIIU.py
+++ GBA2WIIU.py
@@ -19,16 +19,21 @@ def write_file(filename: str, data: (bytes, bytearray)) -> None:
 
 def main() -> None:
 	parser = ArgumentParser(description="A script to migrate saves from GBA to Wii U VC")
-	parser.add_argument("fw", type=str, help="The Wii U save file")
-	parser.add_argument("fg", type=str, help="The GBA save file")
+	parser.add_argument("command", type=str, help="The command you want to use")
+	parser.add_argument("ifile", type=str, help="The Wii U VC save file")
+	# inject only command
+	parser.add_argument("-s", "--save", type=str, help="The save file to inject")
 	parser.add_argument("-o", "--ofile", type=str, help="The file to output to")
 	args = parser.parse_args()
 
+	args.command = args.command.lower()
 	assert isfile(args.fw), "The input Wii U save file doesn't exist!"
-	assert isfile(args.fg), "The input GBA save file doesn't exist!"
+	if args.command == "inject":
+		assert args.save, "-s or --save is required for the inject command!"
+		assert isfile(args.save), "The save to be injected doesn't exist!"
 
 	# make a copy of the file in memory so we don't touch the original
-	with BytesIO(read_file(args.fw)) as bio:
+	with BytesIO(read_file(args.ifile)) as bio:
 		# find the file size
 		bio.seek(0, 2)
 		size = bio.tell()
@@ -52,15 +57,19 @@ def main() -> None:
 		bio.seek(0x80, 1)
 		# grab the save data
 		save_data = bio.read(save_size)
-		# back it up
-		write_file("gba.bin", save_data)
-		# seek to the beginning of the save data
-		bio.seek(-save_size, 1)
-		# read the new save data
-		save_data = read_file(args.fg)
-		assert len(save_data) == save_size, "Save size mismatch!"
-		# write the new save data in
-		bio.write(save_data)
+		if args.command == "extract":
+			print(f"Extracting save @ {hex(bio.tell() - save_size)}, size {hex(save_size)}...")
+			# extract it
+			write_file(args.ofile if args.ofile else "output.bin", save_data)
+		elif args.command == "inject":
+			print(f"Injecting save @ {hex(bio.tell() - save_size)}, size {hex(save_size)}...")
+			# seek to the beginning of the save data
+			bio.seek(-save_size, 1)
+			# read the new save data
+			save_data = read_file(args.save)
+			assert len(save_data) == save_size, "Save size mismatch!"
+			# write the new save data in
+			bio.write(save_data)
 		# get the result
 		data = bio.getvalue()
 
--- README.md
+++ README.md
@@ -4,16 +4,17 @@ This is a script to migrate saves from GBA to the Wii U virtual console
 
 Usage is as follows:
 ```
-usage: GBA2WIIU.py [-h] [-o OFILE] fw fg
+usage: GBA2WIIU.py [-h] [-s SAVE] [-o OFILE] command ifile
 
 A script to migrate saves from GBA to Wii U VC
 
 positional arguments:
-  fw                    The Wii U save file
-  fg                    The GBA save file
+  command               The command you want to use
+  ifile                 The Wii U VC save file
 
 optional arguments:
   -h, --help            show this help message and exit
+  -s SAVE, --save SAVE  The save file to inject
   -o OFILE, --ofile OFILE
                         The file to output to
 ```
